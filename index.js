1106a336c8aecbcc3274f6b3e3a1105fcc24454b
arfuu80 <jyassaryassar@gmail.com>
2024-02-20 21:56:26 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+let inputs = document.getElementById(inp);
+let text = document.querySelector(".text");
+
+function Add(){
+    if(inputs.value==""){
+        alert("Plz Enter Your Task");
+    }
+    else{
+        alert("jhvhhjjv ")                                    
+    }
+}
